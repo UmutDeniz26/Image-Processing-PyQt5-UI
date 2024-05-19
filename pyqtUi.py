a5d25c4f7cce7ab11a3a802aedd5736665069ded
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-05-19 23:34:35 +0300

    Save as ouptut added

--- pyqtUi.py
+++ pyqtUi.py
@@ -1,4 +1,3 @@
-
 # Read PyqtUI.ui file and show the window
 from PyQt5 import QtWidgets, uic
 from PyQt5.QtWidgets import QMainWindow
@@ -20,14 +19,67 @@ class PyqtUI(QMainWindow):
         self.image_operator = Image_Operations.Image_Operations()
         
         # Connect the buttons
-        self.sourceFolder.clicked.connect(self.load_image_button)
-        self.outputSave.clicked.connect(self.save_output_image)
-        self.BGR2Gray.clicked.connect( self.convert_to_gray )
-
+        self.source_folder.clicked.connect(self.load_image_button)
+        self.output_save.clicked.connect(self.save_output_image)
+        self.output_save_as.clicked.connect(self.save_as_output_image)
+        self.bgr_2_gray.clicked.connect(self.convert_to_gray)
+        
         # Connect menu items
-        self.sourceFolder_menu.triggered.connect(self.load_image_button)
+        self.source_folder_menu.triggered.connect(self.source_folder.click)
+        self.output_save_menu.triggered.connect(self.output_save.click)
+        self.output_save_as_menu.triggered.connect(self.output_save_as.click)
+        self.exit_menu.triggered.connect(self.exit_app)
 
-    def update_source_image(self, label_size = (400, 400) ):
+        # Button lists
+        self.menu_buttons = [
+            self.source_folder_menu, self.exit_menu,
+            self.output_save_menu, self.output_save_as_menu, self.output_export_menu, self.source_export_menu,
+            self.source_clear_menu, self.output_clear_menu, self.output_undo_menu, self.output_redo_menu,
+        ]
+
+        # Tools buttons
+        self.tools_buttons = [
+           self.source_folder, self.output_save, self.bgr_2_gray
+        ]
+
+        # Always display buttons
+        self.always_display_buttons = [
+            self.source_folder_menu, self.exit_menu
+        ]
+
+        # Disable the buttons
+        self.disable_buttons()
+        
+
+
+    def display_buttons(self):
+        """
+        Function to display the buttons in the UI
+        
+        Arguments:
+            None
+        Returns:
+            None
+        """
+        for button in self.menu_buttons:
+            if button not in self.always_display_buttons:
+                button.setDisabled(False)
+
+    def disable_buttons(self):
+        """
+        Function to disable the buttons in the UI
+        
+        Arguments:
+            None
+        Returns:
+            None
+        """
+        for button in self.menu_buttons:
+            if button not in self.always_display_buttons:
+                button.setDisabled(True)
+
+
+    def update_source_image(self, label_size=(400, 400)):
         """
         Function to update the source image in the UI
         
@@ -37,14 +89,14 @@ class PyqtUI(QMainWindow):
             None
         """
 
-        self.sourceImage.setPixmap(
+        self.source_image.setPixmap(
             QPixmap.fromImage(
                 self.image_operator.get_source_image().get_QImage()
             ).scaled(label_size[0], label_size[1])
         )
-        self.sourceImage.setAlignment(Qt.AlignCenter)
+        self.source_image.setAlignment(Qt.AlignCenter)
     
-    def update_output_image(self, label_size = (400, 400) ):
+    def update_output_image(self, label_size=(400, 400)):
         """
         Function to update the output image in the UI
 
@@ -54,12 +106,12 @@ class PyqtUI(QMainWindow):
         Returns:
             None
         """ 
-        self.outputImage.setPixmap(
+        self.output_image.setPixmap(
             QPixmap.fromImage(
                 self.image_operator.get_output_image().get_QImage()
             ).scaled(label_size[0], label_size[1])
         )
-        self.outputImage.setAlignment(Qt.AlignCenter)
+        self.output_image.setAlignment(Qt.AlignCenter)
 
     def load_image_button(self):
         
@@ -71,31 +123,44 @@ class PyqtUI(QMainWindow):
             self.image_operator.set_source_image(image_path)            
 
             # Name of the label of the source image is sourceImage
-            self.update_source_image( label_size = ( self.sourceImage.width(), self.sourceImage.height() ) )
+            self.update_source_image(label_size=(self.source_image.width(), self.source_image.height()))
+
+            # Assign the source image path
+            self.source_image_path = image_path
+
+            # Enable the buttons
+            self.display_buttons()
     
     def save_output_image(self):
 
         # Get the folder path
-        folder_path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select folder')
+        #folder_path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select folder')
 
         # If the folder path is not empty, save the output image
-        if folder_path:
-            self.image_operator.get_output_image().save_image(folder_path)
+        if self.source_image_path:
+            self.image_operator.get_output_image().save_image(self.source_image_path)
+
+    def save_as_output_image(self):
+        # Get the folder path
+        image_path = QtWidgets.QFileDialog.getSaveFileName(self, 'Save file', "output.jpg", "Image files (*.jpg *.png)"
+        )[0]
+
+        # If the folder path is not empty, save the output image
+        if image_path:
+            self.image_operator.get_output_image().save_image(image_path)
+
+    def exit_app(self):
+        sys.exit()
+
 
     def convert_to_gray(self):
-        """
-        Function to convert the image to grayscale
-            Arguments:
-                None
-            Returns:
-                None
-        """
-        
         self.image_operator.convert_to_gray()
-        self.update_output_image( label_size = ( self.outputImage.width(), self.outputImage.height() ) )
+        self.update_output_image(label_size=(self.output_image.width(), self.output_image.height()))
+
+
 
 if __name__ == '__main__':
-    print("Testing PyqtUI class:")
+    print("Testing PyqtUI class")
 
     app = QtWidgets.QApplication(sys.argv)
     window = PyqtUI()
