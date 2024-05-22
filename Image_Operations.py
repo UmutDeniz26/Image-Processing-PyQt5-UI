5f3cb7c18fe42311b8c39a3e8a3bb57c78ae693d
UmutDeniz26 <umutcandeniz2101@hotmail.com>
2024-05-22 08:00:48 +0300

    Refactor PyQt UI design for improved usability and aesthetics

--- Image_Operations.py
+++ Image_Operations.py
@@ -2,7 +2,7 @@ import numpy as np
 import cv2
 import skimage as ski
 
-import Image
+from Image import Image
 
 class Image_Operations:
     def __init__(self, image:np.ndarray = None):
@@ -13,74 +13,57 @@ class Image_Operations:
         
         if image is not None:
             self.set_source_image( image )
+
+        # Initialize the image history for redo and undo operations
+        self.source_image_history = {"image_history":[], "current_index":-1}
+        self.output_image_history = {"image_history":[], "current_index":-1}
     
     # Set the source image
     def set_source_image(self, image:np.ndarray):
-        self.source_image = Image.Image(image)
-
-        # When the source image is set, the output image is also reset to the source image
-        self.__set_output_image( self.get_source_image().get_nd_image() ) 
+        self.source_image = Image(image)
+        index = self.source_image_history["current_index"]
+        self.source_image_history["image_history"].insert(index+1, self.source_image)
 
     # Set the output image
     def __set_output_image(self, output:np.ndarray):
-        self.__output_image = Image.Image(output)
+        self.__output_image = Image(output)
+        index = self.output_image_history["current_index"]
+        self.output_image_history["image_history"].insert(index+1, self.__output_image)
     
     # Get the source image
     def get_source_image(self) -> np.ndarray:    
-        return self.source_image
+        index = self.source_image_history["current_index"]
+        return self.source_image_history["image_history"][index]
     
     # Get the output image
     def get_output_image(self) -> np.ndarray:
-        return self.__output_image    
+        index = self.output_image_history["current_index"]
+        return self.output_image_history["image_history"][index] 
 
-
-    def convert_to_gray(self) -> np.ndarray:
-        """
-        Function to convert the image to grayscale
-        
-        Arguments:
-            None
-        Returns:
-            np.ndarray
-
-        """
-
+    # Function to convert the image to a specified color space
+    def convesion_actions(self, method:str='gray') -> np.ndarray:
         src_type = self.get_source_image().get_image_channels_type()
-        if src_type == 'BGR':
-            cvt_type = cv2.COLOR_BGR2GRAY
-        elif src_type == 'GRAY':
-            return
+
+        if method == 'gray':
+            if src_type == 'BGR':
+                cvt_type = cv2.COLOR_BGR2GRAY
+            elif src_type == 'GRAY':
+                return
+            else:
+                raise ValueError('Invalid image type') 
+        elif method == 'hsv':
+            if src_type == 'BGR':
+                cvt_type = cv2.COLOR_BGR2HSV
+            elif src_type == 'GRAY':
+                return
         else:
-            raise ValueError('Invalid image type')
-          
-        self.__set_output_image(cv2.cvtColor(self.get_source_image().get_nd_image(), cvt_type))
-        return self.get_output_image().get_nd_image()
-    
-    def convert_to_hsv(self) -> np.ndarray:
-        """
-        Function to convert the image to HSV
-        :return: np.ndarray
-        """
-        src_type = self.get_source_image().get_image_channels_type()
-        
-        if src_type == 'BGR':
-            cvt_type = cv2.COLOR_BGR2HSV
-        elif src_type == 'GRAY':
-            return
-        else:
-            raise ValueError('Invalid image type')
+            raise ValueError('Invalid method for conversion')
         
         self.__set_output_image(cv2.cvtColor(self.get_source_image().get_nd_image(), cvt_type))
         return self.get_output_image().get_nd_image()
-    
-    def edge_detection(self, method:str='roberts', threshold1:int=100, threshold2:int=200 ) -> np.ndarray:
-        """
-        Function to detect edges in the image
-        :param method: str
-        :param threshold1: int
-        :param threshold2: int
-        :return: np.ndarray
-        """
+
+    # Function to detect edges in the image
+    def edge_detection_actions(self, method:str='roberts', threshold1:int=100, threshold2:int=200 ) -> np.ndarray:
         
         # Check if the image is in grayscale. If not, convert it to grayscale
         if self.get_source_image().get_image_channels_type() == 'BGR':
@@ -98,14 +81,9 @@ class Image_Operations:
         else:
             raise ValueError('Invalid method for edge detection')
     
+    # Function to segment the image
     def segment_image(self, method:str='multi_otsu', num_classes:int=3) -> np.ndarray:
-        """
-        Function to segment the image
-        :param method: str
-        :param num_classes: int
-        :return: np.ndarray
-        """
-
+    
         # Check if the image is in grayscale. If not, convert it to grayscale
         if self.get_source_image().get_image_channels_type() == 'BGR':
             self.set_source_image(self.convert_to_gray())
@@ -120,29 +98,9 @@ class Image_Operations:
         else:
             raise ValueError('Invalid method for segmentation')
         
-    def show_output_image(self):
-        """
-        Function to display the output image
-        """
-        try:
-            cv2.imshow('output', self.get_output_image().get_nd_image())
-            cv2.waitKey(1000)
-            cv2.destroyAllWindows()
-        except:
-            self.__set_output_image(self.get_output_image().get_nd_image().astype(np.uint8))
-            cv2.imshow('output', self.get_output_image().get_nd_image())
-            cv2.waitKey(1000)
-            cv2.destroyAllWindows()
-            
 if __name__ == '__main__':
     print(" Testing image_operator class: ")    
     image = cv2.imread('src/images/lena.png', cv2.IMREAD_COLOR)
     img_op = Image_Operations(image)
-
     img_op.convert_to_gray()
-
-    img_op.show_output_image()
-
-    img_op.edge_detection( method='sobel' )
-
-    img_op.show_output_image()
+    img_op.edge_detection_actions( method='sobel' )
--- pyqtUi.py
+++ pyqtUi.py
@@ -23,95 +23,51 @@ class PyqtUI(QMainWindow):
         self.output_save.clicked.connect(self.save_output_image)
         self.output_save_as.clicked.connect(self.save_as_output_image)
         self.bgr_2_gray.clicked.connect(self.convert_to_gray)
+        self.bgr_2_hsv.clicked.connect(self.convert_to_hsv)
         
         # Connect menu items
         self.source_folder_menu.triggered.connect(self.source_folder.click)
         self.output_save_menu.triggered.connect(self.output_save.click)
         self.output_save_as_menu.triggered.connect(self.output_save_as.click)
         self.exit_menu.triggered.connect(self.exit_app)
+        self.source_clear_menu.triggered.connect(self.clear_source_image)
+
 
         # Button lists
-        self.menu_buttons = [
-            self.source_folder_menu, self.exit_menu,
-            self.output_save_menu, self.output_save_as_menu, self.output_export_menu, self.source_export_menu,
-            self.source_clear_menu, self.output_clear_menu, self.output_undo_menu, self.output_redo_menu,
+        self.all_buttons = [
+            self.source_folder, self.source_folder_menu, self.source_export_menu, self.source_clear_menu
+            ,self.output_save, self.output_save_as,self.output_undo_menu,self.output_save_menu, self.output_save_as_menu, self.output_export_menu,self.output_clear_menu,self.output_redo_menu
+            ,self.bgr_2_gray, self.bgr_2_hsv
+            ,self.exit_menu
         ]
 
         # Tools buttons
         self.tools_buttons = [
-           self.source_folder, self.output_save, self.bgr_2_gray
+           self.source_folder, self.output_save, self.bgr_2_gray, self.bgr_2_hsv
         ]
 
         # Always display buttons
         self.always_display_buttons = [
-            self.source_folder_menu, self.exit_menu
+            self.source_folder,self.source_folder_menu, self.exit_menu
         ]
 
         # Disable the buttons
-        self.disable_buttons()
-        
+        self.change_button_state(self.all_buttons, False)
 
 
-    def display_buttons(self):
-        """
-        Function to display the buttons in the UI
-        
-        Arguments:
-            None
-        Returns:
-            None
-        """
-        for button in self.menu_buttons:
-            if button not in self.always_display_buttons:
-                button.setDisabled(False)
-
-    def disable_buttons(self):
-        """
-        Function to disable the buttons in the UI
-        
-        Arguments:
-            None
-        Returns:
-            None
-        """
-        for button in self.menu_buttons:
-            if button not in self.always_display_buttons:
-                button.setDisabled(True)
 
 
-    def update_source_image(self, label_size=(400, 400)):
-        """
-        Function to update the source image in the UI
-        
-        Arguments:
-            label_size: tuple, (int, int) - The size of the label to display the image
-        Returns:
-            None
-        """
 
-        self.source_image.setPixmap(
-            QPixmap.fromImage(
-                self.image_operator.get_source_image().get_QImage()
-            ).scaled(label_size[0], label_size[1])
-        )
-        self.source_image.setAlignment(Qt.AlignCenter)
-    
-    def update_output_image(self, label_size=(400, 400)):
-        """
-        Function to update the output image in the UI
 
-        Arguments:
-            label_size: tuple, (int, int) - The size of the label to display the image
 
-        Returns:
-            None
-        """ 
-        self.output_image.setPixmap(
-            QPixmap.fromImage(
-                self.image_operator.get_output_image().get_QImage()
-            ).scaled(label_size[0], label_size[1])
-        )
-        self.output_image.setAlignment(Qt.AlignCenter)
+
+
+
+
+
+    #############################################################
+
+    ###################### File Operations ######################
 
     def load_image_button(self):
         
@@ -120,43 +76,99 @@ class PyqtUI(QMainWindow):
         
         # If the image path is not empty, set the source image
         if image_path:
-            self.image_operator.set_source_image(image_path)            
-
             # Name of the label of the source image is sourceImage
+            self.image_operator.set_source_image(image_path)            
             self.update_source_image(label_size=(self.source_image.width(), self.source_image.height()))
 
             # Assign the source image path
             self.source_image_path = image_path
 
             # Enable the buttons
-            self.display_buttons()
+            self.change_button_state(self.all_buttons, True)
     
     def save_output_image(self):
-
-        # Get the folder path
-        #folder_path = QtWidgets.QFileDialog.getExistingDirectory(self, 'Select folder')
-
         # If the folder path is not empty, save the output image
         if self.source_image_path:
             self.image_operator.get_output_image().save_image(self.source_image_path)
 
     def save_as_output_image(self):
         # Get the folder path
-        image_path = QtWidgets.QFileDialog.getSaveFileName(self, 'Save file', "output.jpg", "Image files (*.jpg *.png)"
-        )[0]
+        image_save_path = QtWidgets.QFileDialog.getSaveFileName(
+            self, 'Save file', "output.jpg", "Image files (*.jpg *.png)")[0]
 
         # If the folder path is not empty, save the output image
-        if image_path:
-            self.image_operator.get_output_image().save_image(image_path)
+        if image_save_path:
+            self.image_operator.get_output_image().save_image(image_save_path)
+
+
+
+
+
+
+
+
+
+
+
+
+
+    ##############################################################
+
+    ###################### Image Operations ######################
+
+    # Function to update the source image
+    def update_source_image(self, label_size=(400, 400)):
+        self.source_image.setPixmap(
+            QPixmap.fromImage(
+                self.image_operator.get_source_image().get_QImage()
+            ).scaled(label_size[0], label_size[1])
+        )
+        self.source_image.setAlignment(Qt.AlignCenter)
+    
+    # Function to update the output image
+    def update_output_image(self, label_size=(400, 400)):
+        self.output_image.setPixmap(
+            QPixmap.fromImage(
+                self.image_operator.get_output_image().get_QImage()
+            ).scaled(label_size[0], label_size[1])
+        )
+        self.output_image.setAlignment(Qt.AlignCenter)
+
+    def convert_to_gray(self):
+        self.image_operator.convesion_actions(method='gray')
+        self.update_output_image(label_size=(self.output_image.width(), self.output_image.height()))
+
+    def convert_to_hsv(self):
+        self.image_operator.convesion_actions(method='hsv')
+        self.update_output_image(label_size=(self.output_image.width(), self.output_image.height()))
+
+    def clear_source_image(self):
+        self.image_operator.set_source_image(None)
+        self.source_image_path = None
+        self.change_button_state(self.all_buttons, False)
+        self.source_image.clear()
+
+
+
+
+
+
+
+
+
+
+
+    ##############################################################
+
+    ###################### Common Operations #####################
 
     def exit_app(self):
         sys.exit()
-
-
-    def convert_to_gray(self):
-        self.image_operator.convert_to_gray()
-        self.update_output_image(label_size=(self.output_image.width(), self.output_image.height()))
-
+       
+    def change_button_state(self, button, state):
+        for button in self.all_buttons:
+            if button not in self.always_display_buttons:
+                button.setDisabled(False) if state else button.setDisabled(True)
 
 
 if __name__ == '__main__':
